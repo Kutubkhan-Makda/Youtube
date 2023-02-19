4292b1d01b3f2311ddb4d66b2a7cb13b8e7c93f7
MKhan1792 <kutubkhanmakda1792@gmail.com>
2023-02-19 10:33:54 +0530

    hide

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
     baseURL:"https://www.googleapis.com/youtube/v3/",
     params:{
-        key:"AIzaSyADsUQt_ysuCPQYcOPcgHQjHW2GjXoXDlg",
+        key:process.env.REACT_APP_API_KEY,
     }
 })
 
